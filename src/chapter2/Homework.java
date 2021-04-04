031108dc13e53e8f1f74b469632e59d81180ae30
Armen <simonyanarmen9601@gmail.com>
2021-04-04 23:55:00 +0400

    first commit .

---
+++ src/chapter2/Homework.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+public class Homework {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 4; j >i ; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <i ; k++) {
+                System.out.print("* ");
+            }
+                for (int a = 1; a >0; a--) {
+
+                    System.out.print("*");
+                }
+            System.out.println();
+            }
+
+        }
+    }
+
