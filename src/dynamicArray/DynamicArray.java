b5374c4ad3d3baa083dbb6d4a74337742d79ca8d
Armen <simonyanarmen9601@gmail.com>
2021-05-01 01:59:37 +0400

    Dynamic array update

--- src/dynamicArray/DynamicArray.java
+++ src/dynamicArray/DynamicArray.java
@@ -6,15 +6,16 @@ public class DynamicArray {
     // sa mer masivi mej avelacvac elementneri qanankn e
     private int size = 0;
 
+
     // stugel ete masivi mej tex chka kanchel extand metody ev avelacnel
     public void add(int value) {
-        if (size == array.length - 1) {
+        if (size > array.length - 1) {
             extend();
         }
         array[size] = value;
         size++;
-
     }
+
 
     //stexcel hin masivic  10 element aveli mec masiv
     //qcel hin masivi elementnery nwri mej
@@ -33,7 +34,7 @@ public class DynamicArray {
     public int getByIndex(int index) {
         int value = -1;
         if (index < array.length) {
-             value= array[index];
+            value = array[index];
         }
         return value;
     }
@@ -43,8 +44,85 @@ public class DynamicArray {
         for (int i = 0; i < size; i++) {
             System.out.print(array[i] + " ");
         }
-
+        System.out.println();
     }
 
 
+    // the maximum number
+    public int maximum() {
+        int max = array[0];
+        for (int i = 1; i < size; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+//the minimum number
+
+    public int minimum() {
+        int min = array[0];
+        for (int i = 1; i < size; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public void deleteByIndex(int index) {
+        int[] arr1 = new int[size - 1];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = i > index - 1 ? array[i + 1] : array[i];
+        }
+        size--;
+        array = arr1;
+    }
+
+    public void deleteByValue(int value) {
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                count++;
+            }
+        }
+        for (int i = 0; i < count; i++) {
+            deleteByValueFirst(value);
+
+        }
+
+    }
+
+    public void deleteByValueFirst(int value) {
+        int[] arr = new int[size - 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = array[i];
+            if (arr[i] == value) {
+                for (int j = i; j < arr.length; j++) {
+                    arr[j] = array[j + 1];
+                }
+                break;
+            }
+        }
+        size--;
+        array = arr;
+    }
+
+    public int[] subArray(int startIndex, int endIndex) {
+        int[] subArray = new int[endIndex - startIndex + 1];
+        int j = 0;
+        for (int i = startIndex; i <= endIndex; i++) {
+            subArray[j] = array[i];
+            j++;
+        }
+        return subArray;
+    }
+
+    public int[] subArray(int startIndex) {
+        int[] subArray = new int[size - startIndex];
+        for (int i = startIndex; i < size; i++) {
+            subArray[i - startIndex] = array[i];
+        }
+        return subArray;
+    }
 }
--- src/dynamicArray/DynamicArrayTest.java
+++ src/dynamicArray/DynamicArrayTest.java
@@ -3,14 +3,41 @@ package dynamicArray;
 public class DynamicArrayTest {
     public static void main(String[] args) {
         DynamicArray da = new DynamicArray();
-        for (int i = 0; i < 13; i++) {
-            da.add(i);
-        }
-        int byIndex = da.getByIndex(10);
-        System.out.println(byIndex);
+        da.add(4524);
+        da.add(4);
+        da.add(16);
+        da.add(30);
+        da.add(148);
+        da.add(57);
+        da.add(30);
+        da.add(36);
+        da.add(43);
+        da.add(98);
+        da.add(15);
+        da.add(30);
+        da.add(30);
+        da.add(18);
+        da.add(654);
+        da.add(14);
+        da.add(165);
+        da.add(30);
+        da.add(16);
         da.print();
-
-
+        int max = da.maximum();
+        System.out.println("the maximum number of " + max);
+        int min = da.minimum();
+        System.out.println("the minimum number of " + min);
+        da.deleteByIndex(12);
+        da.print();
+        da.deleteByValueFirst(30);
+        da.print();
+        int[] subArray = da.subArray(3);
+        for (int i : subArray) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        da.deleteByValue(30);
+        da.print();
     }
 
 }
